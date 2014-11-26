2f5abe1ba6ad9d43ac0e5d7aa847f0e048948042
Pocathoughts <christinemoore321@gmail.com>
2014-11-26 18:35:38 -0500

    Improved HSVMask for best 1% contrast, and updated NewApproach.m

--- mFiles/NewApproach.m
+++ mFiles/NewApproach.m
@@ -1,13 +1,23 @@
-figure,
-img = imread('gs3.jpg');
+img = imread('gs2.jpg');
 
 gray = rgb2gray(img);
 Supa_grayfundus = imadjust(gray);
 
+betterFundus = adapthisteq(Supa_grayfundus);
 
-rgbFundus = cat(3, gray, gray, gray);
+rgbFundus = cat(3, Supa_grayfundus, Supa_grayfundus, Supa_grayfundus);
 
-%betterFundus = adapthisteq(gray);
+% %# Create the gaussian filter with hsize = [5 5] and sigma = 2
+% G = fspecial('gaussian',[50 50],2);
+% %# Filter it
+% Ig = imfilter(img, G,'same');
+% %# Display
+% 
+% figure,
+% imshow(Ig);
+% title('Ig');
+
+
 
 % B = [];
 % B(:,:,2)=Supa_grayfundus;
