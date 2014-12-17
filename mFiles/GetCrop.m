8d529f26681c08da7c349cd50e37b25770472d1a
Pocathoughts <christinemoore321@gmail.com>
2014-12-17 12:11:12 -0500

    better crop

--- mFiles/GetCrop.m
+++ mFiles/GetCrop.m
@@ -1,21 +1,57 @@
-function B = GetCrop(img)
-
+function cropped_img = GetCrop(img)
+%%this function helps create a bounding box to be used with the Chenvese active
+%%contours. 
 [x,y]=size(img);
 
 horizontal_middle = x / 2;
+vertical_middle = y / 2;
 top_bound = horizontal_middle - 400;
 bottom_bound = horizontal_middle + 100;
 
-figure, 
-imshow(img);
-title('bounded area');
+% figure, 
+% imshow(img);
+% title('bounded area');
+% 
+% plot([0,y], [top_bound, top_bound],'Color','g','LineWidth',2);
+% plot([0,y], [bottom_bound, bottom_bound],'Color','g','LineWidth',2); 
 
-plot([0,y], [top_bound, top_bound],'Color','g','LineWidth',2);% J = entropyfilt(gray); 
-plot([0,y], [bottom_bound, bottom_bound],'Color','g','LineWidth',2);% J = entropyfilt(gray); 
-% imshow(img), figure, imshow(J,[]); 
 
-B = imcrop(img,[0 top_bound x 800]);
 %B is now the image that we care about
+first_crop = imcrop(img, [0 top_bound x 800]);
+%how does the above line work
+[width, height] = size(first_crop);
+
+disp(width);
+disp(height);
+figure, imshow(first_crop), title('first crop');
+
+bool = getSide(first_crop, horizontal_middle);
+
+%first if statement takes care of the OD on the left side
+if(bool == 0)
+    left_bound = vertical_middle - 500;
+    cropped_img = imcrop(first_crop, [0 0 width left_bound]);
+    %B = imcrop(A,[xmin ymin width height]);
+else if(bool == 1)
+    right_bound = vertical_middle + 500;
+    cropped_img = imcrop(first_crop, [width 0 right_bound height]);
+    %[xmin ymin width height]
+end
+end
+% disp('right bound balue is');
+% disp(left_bound);
+% disp('height value is ');
+% disp(height);
+
+
+
+
+
+
+
+
+
+
 
 % figure,
 % imshow(B);
